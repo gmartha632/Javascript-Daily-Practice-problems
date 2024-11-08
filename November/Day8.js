1af5812efd385e42ea97a14815dfef20dfe7d013
Maruthamuthu_Developer <gmaruthamuthu144@gmail.com>
2024-11-08 18:43:16 +0530

    day8

--- November/Day8.js
+++ November/Day8.js
@@ -1,4 +1,4 @@
-//A number can be said as a strong number when the sum of the factorial of the individual digits is equal to the number. 
+//A number can be said as a strong number when the sum of the factorial of the individual digits is equal to the number.
 //For example, 145 is a strong number. 1! + 4! + 5! 145. Write a program to check whether a given number is a strong number or not
 
 function digitSumFact(n) {
@@ -26,56 +26,91 @@ function factorial(n) {
 }
 digitSumFact(145);
 
-
-
 //You are given an array of numbers.you have to find whether the array is beautiful or not.
 // A beautiful array is an array whose sum of all numbers is divisible by 2, 3 and 5 Output Description: Print 1 if array is beautiful and 0 if it is not Input: arr = [5, 25, 35, -5, 30] output: 1
 function isBeautifulArray(arr) {
-    // Calculate the sum of all elements in the array
-    let sum = 0;
-    for (let num of arr) {
-      sum += num;
+  // Calculate the sum of all elements in the array
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+
+  // Check if sum is divisible by 2, 3, and 5
+  if (sum % 2 === 0 && sum % 3 === 0 && sum % 5 === 0) {
+    console.log(1);
+  } else {
+    console.log(0);
+  }
+}
+isBeautifulArray([5, 25, 35, -5, 30]);
+
+//pattern printing
+
+function hallowSquare(n) {
+  let outputStr = "";
+  if (n == 1) {
+    console.log(1);
+    return;
+  }
+  for (let i = 1; i <= n; i++) {
+    outputStr += "1 ";
+  }
+  console.log(outputStr.trim());
+
+  for (let j = 2; j <= n - 1; j++) {
+    let line = "";
+    for (let k = 1; k <= n; k++) {
+      if (k == 1 || k == n) {
+        line += "*" + " ";
+      } else {
+        line += "  ";
+      }
     }
-  
-    // Check if sum is divisible by 2, 3, and 5
-    if (sum % 2 === 0 && sum % 3 === 0 && sum % 5 === 0) {
-      console.log(1);
+    console.log(line.trim());
+  }
+  outputStr = "";
+  for (let i = 1; i <= n; i++) {
+    outputStr += n + " ";
+  }
+  console.log(outputStr.trim());
+}
+hallowSquare(4);
+
+//Co-prime number
+
+function gcd(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function CoPrime(a, b) {
+  let g = gcd(a, b);
+  if (g == 1) {
+    console.log("Co-Prime");
+  } else {
+    console.log("Not a Prime");
+  }
+}
+CoPrime(4, 7);
+
+//Pattern
+
+function printPattern(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+      continue;
     } else {
-      console.log(0);
+      let outputstr = "";
+      for (let j = 1; j <= i; j++) {
+        outputstr += "*";
+      }
+      console.log(outputstr)
     }
-  }isBeautifulArray([5, 25, 35, -5, 30])
-
-
-  //pattern
-
-  function hallowSquare (n) {
-    let outputStr = "";
-    if (n == 1) {
-        console.log(1);
-        return;
-    }
-    for(let i = 1;i <=n ;i++) {
-        outputStr += "1 "
-    }
-    console.log(outputStr.trim());
-
-    for(let j = 2; j <=n-1; j++) {
-        let line = "";
-        for(let k = 1; k <= n; k++) {
-            if(k == 1 || k == n) {
-                line += "*" + " ";
-            }
-            else {
-                line += "  ";
-            }
-        }
-        console.log(line.trim());
-    }
-    outputStr = "";
-    for(let i = 1;i <=n ;i++) {
-        outputStr += n + " ";
-    }
-    console.log(outputStr.trim());
-
-}hallowSquare(4)
+  }
+}printPattern(4)
 
