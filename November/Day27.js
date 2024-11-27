017c21bfaa7dc8102a12232360f5e13521fe03e4
Maruthamuthu_Developer <gmaruthamuthu144@gmail.com>
2024-11-28 00:02:10 +0530

    day27

--- November/Day27.js
+++ November/Day27.js
@@ -1,4 +1,12 @@
-//
+//For the first 100 kWh, the rate is ₹8 per kWh.
+//For the next 200 kWh (from 101 to 300 kWh), the rate is ₹12 per kWh.
+//For any usage above 300 kWh, the rate is ₹15 per kWh.
+//First 100 kWh = 100 × ₹8 = ₹800
+//Next 200 kWh = 200 × ₹12 = ₹2400
+//Remaining 50 kWh = 50 × ₹15 = ₹750
+//Total Bill = ₹800 + ₹2400 + ₹750 = 3950
+//The program takes an array of usages, the program prints the sum
+//example usages = [350, 150, 100];
 function calculateBill(usage) {
     let bill = 0;
   
@@ -22,5 +30,24 @@ function calculateBill(usage) {
     }
   
     console.log(totalBill);
-  }calculateTotalBill([350, 150, 100])
+  }calculateTotalBill([350, 150, 100]);
 
+// 1. Problem: Given an array of integers, count how many numbers are even and how many are odd.
+
+// Example Input: [1, 2, 3, 4, 5, 6]
+// Example Output: { even: 3, odd: 3 }
+
+
+function evenOdd(arr){
+  let  count = 0
+  let  total = 0
+  for(let i = 0 ; i< arr.length ;i++){
+      if(arr[i] % 2 == 0){
+          count++
+      }
+      else if( arr[i] % 2 !== 0){
+          total++
+      }
+  }
+  console.log(`{even:${count} , odd:${total}}`)
+}evenOdd([1, 2, 3, 4, 5, 6,7])
