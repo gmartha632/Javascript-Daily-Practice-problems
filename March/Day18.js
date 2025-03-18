19ebf5c69fff98c861a3c46a842adfcadc358dbc
gmartha632 <gmaruthamuthu144@gmail.com>
2025-03-18 23:03:19 +0530

    Sales by match problem solved by hashmap method

---
+++ March/Day18.js
@@ -0,0 +1,26 @@
+// Sales by Match Implementation
+
+function sockMerchant(n, ar) {
+  let countMap = {};
+  for (let i = 0; i < n; i++) {
+    // Constructing the countMap
+    if (countMap[ar[i]]) {
+      countMap[ar[i]] = countMap[ar[i]] + 1;
+    } else {
+      countMap[ar[i]] = 1;
+    }
+  }
+
+  // Calculating total pairs of socks in stock
+  let count = 0;
+  for (let k in countMap) {
+    let a = Math.floor(countMap[k] / 2);
+    count += a;
+  }
+
+  console.log(`Total pairs of Socks in stock ${count}`);
+}
+sockMerchant(7, [1, 2, 1, 2, 1, 3, 2]);
+
+
+
